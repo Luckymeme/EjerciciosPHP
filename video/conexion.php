7860d2b9ac54c35578cb49bee72c70db915fee57
Rafael <57674238+Luckymeme@users.noreply.github.com>
2025-04-09 15:59:45 +0200

    tutorial

---
+++ video/conexion.php
@@ -0,0 +1,9 @@
+<?php
+try {
+    $base = new PDO('mysql:host=localhost; bdname=pruebas', 'root', '');
+    $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $base->exec("SET CHARACTER SET UTF8");
+} catch (Exception $e) {
+    die('Error' . $e->getMessage());
+    echo 'Línea del error' . $e->getLine();
+}
