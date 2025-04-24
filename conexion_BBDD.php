4f63e5f3f4914b9f4d802d2431069185b9820d70
Rafael <57674238+Luckymeme@users.noreply.github.com>
2025-04-24 20:13:29 +0200

    ligeros cambios 24/04/25

--- conexion_BBDD.php
+++ conexion_BBDD.php
@@ -26,7 +26,8 @@
 
     // $consulta = "SELECT * FROM DATOSPERSONALES";
     $consulta = "SELECT * FROM PRODUCTOS";
-    $filtro = "SELECT * FROM PRODUCTOS WHERE PAÍSDEORIGEN='ESPAÑA'";
+    // $filtro = "SELECT * FROM hoja1 WHERE PAÍSDEORIGEN='ESPAÑA'";
+    // $consulta = "SELECT * FROM PRODUCTOS WHERE PAÍSDEORIGEN='ESPAÑA'";
 
 
     $resultados = mysqli_query($conexion, $consulta); //BBDD y consulta a realizar = tabla virtual record set o result set.
@@ -35,7 +36,9 @@
     $space = "&nbsp&nbsp&nbsp&nbsp"; //es cutre no lo siguiente, pero parece que es lo mejor qua hay \n o \t no hacen nada.
     // UPDATE: str_repeat($space, 5) puede valer tambien
     while ($fila = mysqli_fetch_row($resultados)) {
-        echo $fila[0] . str_repeat($space, 2) . $fila[1] . $space . $fila[2] . $space . $fila[3] . $space . "<br>";
+        echo "<table width='50%' align='center' border ='1'><tr><td>"; //12:36 video  38
+
+        echo $fila[0] . "</td><td>" . str_repeat($space, 2) . $fila[1] . "</td><td>" . $space . $fila[2] . $space . "</td><td>" .  $fila[3] . "</tr><td></tr></table><br>";
     }
 
 
