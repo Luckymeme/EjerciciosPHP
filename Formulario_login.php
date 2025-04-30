a74121fd810fcae99c65c3981b0a3ad91e8e200e
Rafael <57674238+Luckymeme@users.noreply.github.com>
2025-04-30 20:03:07 +0200

    30/04/2025
    video 48

---
+++ Formulario_login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de busqueda</title>
+</head>
+
+<body>
+    <form action="pagina_datos.php" method="get">
+
+        <label for="">Usuario: <input type="text" name="usu"></label><br>
+        <label for="">Contraseña: <input type="text" name="con"></label>
+        <input type="submit" name="enviando" value="Login">
+
+    </form>
+
+</body>
+
+</html>
--- pagina_datos.php
+++ pagina_datos.php
@@ -5,13 +5,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pagina busqueda</title>
+    <!-- 48 -->
 </head>
 
 <body>
     <?php
-    $busqueda = $_GET["buscar"];
+    $usuario = $_GET["usu"];
+    $contra = $_GET["con"];
 
     require("datos_conexion.php");
+
     $conexion = mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
     if (mysqli_connect_errno()) {
         echo "Fallo al concetar con la BBDD";
@@ -23,7 +26,7 @@
 
     // $consulta = "SELECT * FROM PRODUCTOS WHERE NOMBRE LIKE'%$busqueda%'"; //Es AQUI; genio, crack, mastodonte
 
-    $consulta = "SELECT * FROM PRODUCTOS WHERE NOMBRE = '$busqueda'";
+    $consulta = "SELECT * FROM USUARIOS WHERE USUARIO = '$usuario' AND CONTRA = '$contra'";
     echo "$consulta <br><br>";
 
     $resultados = mysqli_query($conexion, $consulta);
@@ -31,13 +34,13 @@
     $space = "&nbsp&nbsp&nbsp&nbsp"; //es cutre no lo siguiente, pero parece que es lo mejor qua hay \n o \t no hacen nada.
     // UPDATE: str_repeat($space, 5) puede valer tambien
     while ($fila = mysqli_fetch_array($resultados, MYSQLI_ASSOC)) {
+        echo "Bienvenid@ <b>$usuario</b> <br> Estos son tus datos: <br>";
+
         echo "<table width='50%' align='center' border ='1'><tr><td>";
-        echo $fila['CÓDIGOARTÍCULO'] . str_repeat($space, 1) . "</td><td>"
-            . $fila['NOMBRE'] . $space . "</td><td>"
-            . $fila['SECCIÓN'] . $space . "</td><td>"
-            . $fila['IMPORTADO'] . $space . "</td><td>"
-            . $fila['PRECIO'] . $space . "</td><td>"
-            . $fila['PAÍSDEORIGEN'] . $space . "</tr><td></tr></table><br>";
+        echo $fila['usuario'] . str_repeat($space, 1) . "</td><td>"
+            . $fila['contra'] . $space . "</td><td>"
+            . $fila['tfno'] . $space . "</td><td>"
+            . $fila['direccion'] . $space . "</tr><td></tr></table><br>";
     }
     mysqli_close($conexion);
 
